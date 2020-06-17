7bb3c73aa58b8fc3e05315a246f5a9d9f185a001
kennedyt71079 <58008618+kennedyt71079@users.noreply.github.com>
2020-06-17 12:45:42 +1200

    legendary guess counter

--- Final Higher Lower.py
+++ Final Higher Lower.py
@@ -5,7 +5,6 @@ import random
 # ask for values
 lowest = int(input("Please enter a low number: "))
 highest = int(input("Please enter a high number: "))
-rounds = int(input("How many rounds do you want to play: "))
 guesses_allowed = int(input("How many guesses do you want: "))
 
 # Generate random number
@@ -13,8 +12,7 @@ random_num = random.randrange(lowest, highest)
 
 # initialise variables
 already_guessed = []
-rounds_played = 0
-num_won = 0
+guesses = 0
 guess = ""
 
 # start game
@@ -39,20 +37,14 @@ while guess != random_num and guesses_allowed >= 1:
 
         elif guess > random_num:
             print("vvv Too high, try a lower number. Guesses left: {} vvv".format(guesses_allowed))
-    else:
-        if guess < random_num:
-            print("^^^ Too low, try a higher number. Guesses left: {} ^^^".format(guesses_allowed))
-        elif guess > random_num:
-            print("vvv Too high, try a lower number. Guesses left: {} vvv".format(guesses_allowed))
+    guesses += 1
 
 if guess == random_num:
     if guesses_allowed == guesses_allowed - 1:
         print("!!!Amazing! you got it in one guess!!!")
     else:
-        print("!!!Well done, you got it in {} guesses!!!".format(guesses_allowed))
-    num_won += 1
+        print("!!!Well done, you got it in {} guesses!!!".format(guesses))
+
 else:
     print("Sorry, you lose this round as you have run out of guesses")
-
-    print("Won: {} \t | \t Lost: {}".format(num_won, rounds_played - num_won + 1))
-    rounds_played +=1
+    print("The number was {}".format(random_num))
